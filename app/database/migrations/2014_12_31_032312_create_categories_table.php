a955c9e2f9d18b0177e9afed1383f970221aa73f
David Hemphill <davidlee.hemphill@gmail.com>
2014-12-30 22:15:32 -0600

    Whoops!

--- app/database/migrations/2014_12_31_032312_create_categories_table.php
+++ app/database/migrations/2014_12_31_032312_create_categories_table.php
@@ -14,7 +14,7 @@ class CreateCategoriesTable extends Migration {
 	{
 		Schema::create('categories', function(Blueprint $table)
 		{
-			$table->increments('title');
+			$table->increments('id');
 			$table->string('title');
 			$table->string('slug');
 		});
