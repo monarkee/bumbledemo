6d4b22c6245520d14885fa6db6271e1d0bb331d0
David Hemphill <davidlee.hemphill@gmail.com>
2015-01-01 22:31:24 -0600

    Fix migration

--- app/database/migrations/2014_12_31_032042_create_posts_table.php
+++ app/database/migrations/2014_12_31_032042_create_posts_table.php
@@ -17,14 +17,13 @@ class CreatePostsTable extends Migration {
 			$table->increments('id');
 			$table->string('title');
 			$table->string('slug');
-			$table->text('excerpt');
+			$table->text('excerpt')->nullable();
 			$table->text('content');
-			$table->tinyInteger('active');
-			$table->integer('sort');
-			$table->timestamp('published_at');
-			$table->timestamp('created_at');
-			$table->timestamp('updated_at');
-			$table->timestamp('deleted_at');
+			$table->tinyInteger('active')->nullable()->default(0);
+			$table->integer('sort')->nullable();
+			$table->datetime('published_at');
+			$table->timestamps();
+			$table->softDeletes();
 		});
 	}
 
@@ -35,7 +34,7 @@ class CreatePostsTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('categories');
+		Schema::drop('posts');
 	}
 
 }
